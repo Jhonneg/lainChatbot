a5f2145fa95e799e20e009fce9c84ef534b08f4e
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-10-07 09:38:45 -0300

    added github svg anchor link

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bg-no-repeat bg-right-bottom bg-black w-full h-full font-sans">
-    <div class="flex items-center justify-center h-full">
+    <div class="flex flex-col items-center justify-center h-full">
       <div
         class="max-w-[800px] relative z-20 shadow-lg after:content-[' '] after:z-[-1] after:h-full after:bg-slate-900 after:border after:border-slate-900 after:absolute after:left-2.5 after:right-2.5 after:top-0 after:mt-2 after:rounded"
       >
@@ -10,6 +10,16 @@
 
           <!-- Chat Screen -->
           <Chat v-else />
+          <p class="text-gray-300 text-center mt-8">
+            Made by
+            <a href="https://github.com/Jhonneg">
+              <img
+                class="inline mx-1"
+                width="30"
+                src="/public/github-mark-white.svg"
+                alt="github"
+            /></a>
+          </p>
         </div>
       </div>
     </div>
